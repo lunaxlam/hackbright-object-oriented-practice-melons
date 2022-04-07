6ffa534cb0715f1cf1c9e280ae805c96235fdb6c
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-06 19:53:36 -0700

    Initial commit

---
+++ harvest.py
@@ -0,0 +1,78 @@
+############
+# Part 1   #
+############
+
+
+class MelonType:
+    """A species of melon at a melon farm."""
+
+    def __init__(
+        self, code, first_harvest, color, is_seedless, is_bestseller, name
+    ):
+        """Initialize a melon."""
+
+        self.pairings = []
+
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
+
+
+    def add_pairing(self, pairing):
+        """Add a food pairing to the instance's pairings list."""
+
+        # Fill in the rest
+
+    def update_code(self, new_code):
+        """Replace the reporting code with the new_code."""
+
+        # Fill in the rest
+
+
+def make_melon_types():
+    """Returns a list of current melon types."""
+
+    all_melon_types = []
+
+    # Fill in the rest
+
+    return all_melon_types
+
+
+def print_pairing_info(melon_types):
+    """Prints information about each melon type's pairings."""
+
+    # Fill in the rest
+
+
+def make_melon_type_lookup(melon_types):
+    """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
+
+    # Fill in the rest
+
+
+############
+# Part 2   #
+############
+
+
+class Melon:
+    """A melon in a melon harvest."""
+
+    # Fill in the rest
+    # Needs __init__ and is_sellable methods
+
+
+def make_melons(melon_types):
+    """Returns a list of Melon objects."""
+
+    # Fill in the rest
+
+
+def get_sellability_report(melons):
+    """Given a list of melon object, prints whether each one is sellable."""
+
+    # Fill in the rest
